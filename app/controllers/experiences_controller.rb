bfd9a1eeb1fc416191832db1e32cfc8ab1314a04
tattu501 <61368201+tattu501@users.noreply.github.com>
2020-10-09 00:47:07 +0900

    Develop content of tennis experience

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,4 +1,48 @@
 class ExperiencesController < ApplicationController
   def new
+    @experience = Experience.new
   end
+
+  def create
+    @experience = Experience.new(experience_params)
+    if @experience.save
+      flash[:success] = "体験記の登録に成功しました！"
+      redirect_to @experience
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @experiences = Experience.select('id', 'year', 'month')
+  end
+
+  def show
+    @experience = Experience.find(params[:id])
+  end
+
+  def edit
+    @experience = Experience.find(params[:id])
+  end
+  
+  def update
+    @experience = Experience.find(params[:id])
+    if @experience.update(experience_params)
+      flash[:success] = "体験記が更新されました！"
+      redirect_to @experience
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Experience.find(params[:id]).destroy
+    flash[:success] = "体験記が削除されました。"
+    redirect_to experiences_url
+  end
+
+  private
+    def experience_params
+      params.require(:experience).permit(:year, :month, :explanation, :image)
+    end
 end
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -3,4 +3,5 @@ class Experience < ApplicationRecord
   default_scope { order(year: :desc, month: :desc) }
   validates :year,  presence: true, length: { minimum: 1 }
   validates :month, presence: true, length: { in: 1..2 }
+  validates :explanation, presence: true, length: { minimum: 50 }
 end
