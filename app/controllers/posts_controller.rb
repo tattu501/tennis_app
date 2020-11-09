79d9b3ece646c5c2996f66b20debeea1e429593c
tattu501 <61368201+tattu501@users.noreply.github.com>
2020-11-09 23:16:22 +0900

    posts_controller.rbにnew, createの2メソッドを追加
    新規投稿機能を作成するため

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:notice] = "登録しました"
+      redirect_to root_url
+    else
+      render 'posts/new'
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:content).merge(user_id: current_user.id)
+    end
 end
