a9af60092b0cd911123ef5c085bcb38b0a732bf9
tattu501 <61368201+tattu501@users.noreply.github.com>
2020-11-12 00:11:31 +0900

    comments_controller.rbを修正
    edit/update/destroyの3アクションを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,14 @@
 class CommentsController < ApplicationController
-  before_action :logged_in_user
+  before_action :logged_in_user, only: [:new, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update, :destroy]
+  before_action :set_post
+
+  def edit
+    @comment = Comment.find(params[:id])
+    @comments = @post.comments.includes(:user).order("created_at DESC")
+  end
 
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     if @comment.save
       redirect_to post_path(@post.id)
@@ -12,8 +18,35 @@ class CommentsController < ApplicationController
     end
   end
 
+  def update
+    if @comment.update_attributes(comment_params)
+      redirect_to post_path(@comment.post_id)
+    else
+      flash[:alert] = "コメントを(140文字以内で)入力してください。"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:comment).merge(user_id: current_user.id)
+    end
+
+    def authenticate_admin!
+      @comment = Comment.find(params[:id])
+      
+      if @comment.user_id != current_user.id
+        flash[:notice] = "権限はありません"
+        redirect_to post_path(@comment.post_id)
+      end
+    end
+
+    def set_post
+      @post = Post.find(params[:post_id])
     end
 end
