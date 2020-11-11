6f077b0c31a80a5b37981de92a21df178a0497e2
tattu501 <61368201+tattu501@users.noreply.github.com>
2020-11-11 22:42:15 +0900

    posts_controller.rbを修正
    update、destroyアクションでのエラーを解消

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update, :destroy]
+
   def new
     @post = Post.new
   end
@@ -27,20 +30,30 @@ class PostsController < ApplicationController
   def update
     if @post.update_attributes(post_params)
       flash[:notice] = "編集しました"
-      redirect_to root_url
+      redirect_to posts_path
     else
       render 'posts/edit'
     end
   end
 
   def destroy 
-    @post.destroy
+    Post.find(params[:id]).destroy
     flash[:notice] = "削除しました"
-    redirect_to root_url
+    redirect_to posts_path
   end
 
   private
     def post_params
       params.require(:post).permit(:content).merge(user_id: current_user.id)
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @post = Post.find(params[:id])
+      
+      if @post.user_id != current_user.id
+        flash[:notice] = "権限はありません"
+        redirect_to root_url
+      end
     end
 end
