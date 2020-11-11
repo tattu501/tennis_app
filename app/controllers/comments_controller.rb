943566813d17db13884ab6bc7ace9c76135d9d75
tattu501 <61368201+tattu501@users.noreply.github.com>
2020-11-11 23:48:24 +0900

    comments_controller.rbにcreateアクションを追加
    コメント登録のため

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  before_action :logged_in_user
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@post.id)
+    else
+      flash[:alert] = "コメントを(140文字以内で)入力してください。"
+      redirect_to post_path(@post.id)
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:comment).merge(user_id: current_user.id)
+    end
 end
