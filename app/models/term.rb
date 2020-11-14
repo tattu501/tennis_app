d9b38b720b1918c2ea9cbac94abb98a796fa23da
tattu501 <61368201+tattu501@users.noreply.github.com>
2020-11-14 23:26:05 +0900

    term.rbを修正
    termの1文字以上制限を削除

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,6 +1,6 @@
 class Term < ApplicationRecord
   has_one_attached :image
   default_scope { order(term: :asc) }
-  validates :term,  presence: true, uniqueness: true, length: { minimum: 1 }
+  validates :term,  presence: true, uniqueness: true
   validates :explanation, presence: true
 end
