336d49458f085d66fc7544f93487a0706d415749
tattu501 <61368201+tattu501@users.noreply.github.com>
2020-11-14 22:52:21 +0900

    term.rbを修正
    バリデーションを修正
    ・explanationの10文字以上制限を廃止

--- app/models/term.rb
+++ app/models/term.rb
@@ -2,5 +2,5 @@ class Term < ApplicationRecord
   has_one_attached :image
   default_scope { order(term: :asc) }
   validates :term,  presence: true, uniqueness: true, length: { minimum: 1 }
-  validates :explanation, presence: true, length: { minimum: 10 }
+  validates :explanation, presence: true
 end
