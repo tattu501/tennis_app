fb7d09de33536a4c0efc5efbd6979de4e42162de
tattu501 <61368201+tattu501@users.noreply.github.com>
2020-11-15 23:38:29 +0900

    term.rbを修正
    画像の容量制限とリサイズ済み画像を返却するメソッドを追加

--- app/models/term.rb
+++ app/models/term.rb
@@ -3,4 +3,13 @@ class Term < ApplicationRecord
   default_scope { order(term: :asc) }
   validates :term,  presence: true, uniqueness: true
   validates :explanation, presence: true
+  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+                                      message: "must be a valid image format" },
+                      size:         { less_than: 5.megabytes,
+                                      message: "should be less than 5MB" }
+
+  # 表示用のリサイズ済み画像を返す
+  def display_image
+    image.variant(resize_to_limit: [600, 300])
+  end
 end
