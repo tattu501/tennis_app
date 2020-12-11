21c2e4c8d4bf72af2631532f430d8ce442d932b6
tattu501 <61368201+tattu501@users.noreply.github.com>
2020-12-11 22:30:56 +0900

    Update post_spec.rb
    postモデルのテスト

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Post do
+RSpec.describe Experience do
   describe '#validation' do
     context 'can save' do
-      it "is valid with a content, user_id" do
+      it "is valid with a date, explaination" do
         post = build(:post)
         expect(post).to be_valid
       end
